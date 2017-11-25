3bfa4ad0a4fb5cf66a423653615ebb7dee441fff
amytruong <truong.amy27@gmail.com>
2017-11-25 11:54:54 -0600

    All models work for Ford

--- carProject2.py
+++ carProject2.py
@@ -3,6 +3,11 @@
 
 def pickCar():
 
+    make = ""
+    model = ""
+    option = ""
+    price = ""
+
     def pickOption(baseprice):
         options = int(input("""You can upgrade your vehicle:\n1) Standard -- (base price)\n2) Sports -- + $3,000
               \n3) Luxury -- + $6,000\n"""))
@@ -22,32 +27,31 @@ def pickCar():
             return option, price
 
 
-    make = ""
-    model = ""
-    option = ""
-    price = ""
+
     userMake = int(input("""Please choose a make:\n1) Ford\n2) Chevrolet\n3) Toyota\n"""))
     if (userMake == 1):
         make = "Ford"
         fordModel = int(input("""Please choose a model:\n1) F-150\n2) Focus\n3) Mustang\n"""))
+
+
         if (fordModel == 1):
             model = "F-150"
-            f150baseprice = 27380
+            f150Baseprice = 27380
             print("The base price of a 2018 Ford F-150 is: $27,380")
-            option, price = pickOption(f150baseprice)
+            option, price = pickOption(f150Baseprice)
 
         elif (fordModel == 2):
+            model = "Focus"
+            focusBaseprice = 17860
             print("The base price of a 2018 Ford Focus is: $17,860")
-            focusOptions = int(input("""You can upgrade your Focus.
-            1) Standard Sedan -- (base price)
-            2) Titanium Sedan -- $24,175
-            3) Electric -- $29,120"""))
+            option, price = pickOption(focusBaseprice)
+
         elif (fordModel == 3):
+            model = "Mustang"
+            mustangBaseprice = 25585
             print("The base price of a 2018 Ford Mustang is: $25,585")
-            mustangOptions = int(input("""You can upgrade your Mustang.
-            1) Standard EcoBoost -- (base price)
-            2) EcoBoost Convertible -- $31,085
-            3) Shelby GT350 -- $57,145"""))
+            option, price = pickOption(mustangBaseprice)
+
 
     print("Congrats! You've picked a " +make, model, option, "for " + str(price))
     fileHandle = open("customer.txt", "a")
